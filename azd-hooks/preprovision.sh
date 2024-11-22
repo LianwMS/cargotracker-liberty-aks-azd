d0cba98aee114558ed52348d79804273b10929e9
zhihaoguo <zhihaoguo@microsoft.com>
2024-11-22 14:30:48 +0800

    Remove unnecessary steps from the preprovision scripts for clarity.

--- azd-hooks/preprovision.sh
+++ azd-hooks/preprovision.sh
@@ -38,37 +38,3 @@ fi
 echo -e "\n✅ All required tools are installed!"
 
 az extension add --upgrade -n application-insights
-source .scripts/setup-env-variables-template.sh
-
-mkdir tmp-build
-DIR=$(pwd)/tmp-build
-echo "Current directory: $DIR"
-
-cd ${DIR}
-git clone https://github.com/WASdev/azure.liberty.aks ${DIR}/azure.liberty.aks
-
-cd ${DIR}/azure.liberty.aks
-git checkout ${LIBERTY_AKS_REPO_REF}
-export VERSION=1.0.22
-
-cd ${DIR}
-curl -L -o ${DIR}/azure-javaee-iaas-parent-${VERSION}.pom  \
-     https://github.com/azure-javaee/azure-javaee-iaas/releases/download/azure-javaee-iaas-parent-${VERSION}/azure-javaee-iaas-parent-${VERSION}.pom
-
-
-mvn install:install-file -Dfile=${DIR}/azure-javaee-iaas-parent-${VERSION}.pom \
-                         -DgroupId=com.microsoft.azure.iaas \
-                         -DartifactId=azure-javaee-iaas-parent \
-                         -Dversion=${VERSION} \
-                         -Dpackaging=pom
-
-cd ${DIR}/azure.liberty.aks
-mvn clean package -DskipTests
-
-mkdir -p ${DIR}/../infra/azure.liberty.aks
-cp -r ${DIR}/azure.liberty.aks/target/bicep/* ${DIR}/../infra/azure.liberty.aks
-
-# shell sleep 5 seconds
-sleep 5
-
-rm -rf ${DIR}
